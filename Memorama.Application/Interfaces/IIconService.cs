ec68895f984564e2fe517721b82ed58a6bba13f9
brauliopy-lap <balcantararomo@BRAULIOLAP>
2026-05-08 08:21:08 -0600

    Se creo el Servicio de Consulta de Iconos, asi como la incrustacion de los archivos fuente de googleMaterialIcon para los mismos

---
+++ Memorama.Application/Interfaces/IIconService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memorama.Application.Interfaces
+{
+    public interface IIconService
+    {
+        // Pedimos una lista de strings (ej. "home", "favorite")
+        List<string> ObtenerNombresDeIconos(int cantidad);
+    }
+}
---
+++ Memorama.Infrastructure/Services/IconService.cs
@@ -0,0 +1,41 @@
+﻿using Memorama.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Memorama.Infrastructure.Services
+{
+    public class IconService : IIconService
+    {
+        public List<string> ObtenerNombresDeIconos(int cantidad)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            // AJUSTA ESTO: NombreProyecto.Carpeta.Archivo
+            var resourceName = "Memorama.Infrastructure.Resources.iconsManager.txt";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string jsonFull = reader.ReadToEnd();
+                // Limpiamos el prefijo de seguridad de Google
+                string jsonLimpio = jsonFull.Replace(")]}'", "");
+
+                using (JsonDocument doc = JsonDocument.Parse(jsonLimpio))
+                {
+                    var icons = doc.RootElement.GetProperty("icons");
+                    var todosLosNombres = icons.EnumerateArray()
+                        .Select(icon => icon.GetProperty("name").GetString())
+                        .ToList();
+
+                    Random rnd = new Random();
+                    return todosLosNombres.OrderBy(x => rnd.Next()).Take(cantidad).ToList();
+                }
+            }
+        }
+    }
+}
