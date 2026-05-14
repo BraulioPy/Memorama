ade28c4ceef9b1173c1d1fa4a69e77ecea280b07
brauliopy-lap <balcantararomo@BRAULIOLAP>
2026-05-13 22:41:26 -0600

    Se creó la interfaz y el servicio de sonidos

---
+++ Memorama.Application/Interfaces/ISoundService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memorama.Application.Interfaces
+{
+    public interface ISoundService
+    {
+        void PlayFlip();
+        void PlayFlippingCards();
+        void PlayWin();
+        void PlayGameOver();
+        void StopMusic();
+    }
+}
---
+++ Memorama.Infrastructure/Services/SoundService.cs
@@ -0,0 +1,39 @@
+﻿using Memorama.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace Memorama.Infrastructure.Services
+{
+    public class SoundService : ISoundService
+    {
+        // Accedemos a los recursos internos de la capa de Infrastructure
+        private readonly SoundPlayer _flipSound;
+        private readonly SoundPlayer _WinSound;
+        private readonly SoundPlayer _loseSound;
+        private readonly SoundPlayer _flippingSound;
+
+        public SoundService()
+        {
+            // Properties.Resources apuntará a los archivos que metiste en esta capa
+            _flipSound = new SoundPlayer(Properties.Resources._flip);
+            _WinSound = new SoundPlayer(Properties.Resources._win);
+            _loseSound = new SoundPlayer(Properties.Resources._lose);
+            _flippingSound = new SoundPlayer(Properties.Resources._cardsFlipping);
+        }
+        public void PlayFlip() => _flipSound.Play();
+        public void PlayFlippingCards() => _flippingSound.Play();
+        public void PlayWin() => _WinSound.Play();
+        public void PlayGameOver() => _loseSound.Play();
+        public void StopMusic() { 
+            _flipSound.Stop();
+            _WinSound.Stop();
+            _loseSound.Stop();
+            _flippingSound.Stop();
+        }
+
+    }
+}
